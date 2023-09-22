3ed225c7d55ded8a482bba7ae1f1c90065af8c7e
bideoncenten <145735291+bideoncenten@users.noreply.github.com>
2023-09-22 02:48:24 +0000

    up

---
+++ t-a-p-p-y-t-o-o-n.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         t-a-p-p-y-t-o-o-n
+// @namespace    https://github.com/bideoncenten/ngoai_sau_muoi/raw/main/t-a-p-p-y-t-o-o-n.user.js
+// @version      2023.09.21
+// @description  down t-a-p-p-y-t-o-o-n
+// @author       You
+// @match        https://www.tappytoon.com/en/chapters/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=www.tappytoon.com
+// @require      https://unpkg.com/file-saver@2.0.5/dist/FileSaver.min.js
+// @grant        none
+// @downloadURL  https://github.com/bideoncenten/ngoai_sau_muoi/raw/main/t-a-p-p-y-t-o-o-n.user.js
+// @updateURL    https://github.com/bideoncenten/ngoai_sau_muoi/raw/main/t-a-p-p-y-t-o-o-n.meta.js
+// ==/UserScript==
